01861180dd9e53268e67da9efcb0426c09d2f631
Thibault Nocchi <thibaultnocchi@gmail.com>
2018-12-21 09:13:27 +0100

    Q10

---
+++ src/drawingeditor/model/Ell.java
@@ -0,0 +1,11 @@
+package drawingeditor.model;
+
+import javafx.scene.paint.Paint;
+
+public class Ell extends FormeImpl {
+    
+    public Ell(double x, double y, double w, double h, Paint color) {
+        super(x, y, w, h, color);
+    }
+
+}
---
+++ src/drawingeditor/model/Rect.java
@@ -0,0 +1,11 @@
+package drawingeditor.model;
+
+import javafx.scene.paint.Paint;
+
+public class Rect extends FormeImpl {
+
+    public Rect(double x, double y, double w, double h, Paint color){
+        super(x,y,w,h,color);
+    }
+
+}
